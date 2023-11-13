7bdbc17f679ca6fb227442d318af11b3967ca180
ekankam <eb.kankam26@gmail.com>
2023-11-13 16:58:54 -0600

    Add phone field validation

--- components/sections/PersonalInfo.tsx
+++ components/sections/PersonalInfo.tsx
@@ -20,11 +20,13 @@ import useStore from "@/store/useStore";
 import Container from "../Container";
 
 const formSchema = z.object({
-  name: z.string().min(5, { message: "Name is required." }).max(100),
+  name: z.string().min(5, { message: "Name is required" }).max(100),
   email: z.string().min(1, { message: "Email is required" }).email({
     message: "Must be a valid email",
   }),
-  phone: z.string().min(10, { message: "Phone is required" }),
+  phone: z.string().refine((val) => /^\d{10}$/.test(val), {
+    message: "Phone is required",
+  }),
 });
 
 type ValidationSchema = z.infer<typeof formSchema>;
